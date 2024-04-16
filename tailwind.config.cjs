35f6d75c8cb0fb62215901249ca5a286fc13840d
unknown <bmahn@knou.ac.kr>
2024-04-16 09:49:40 +0900

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	content: [
+		'./slices/**/*.{js,ts,vue}'
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				body: ['Nunito Sans Variable', 'sans-serif'],
+				heading: ['Nunito Variable', 'sans-serif'],
+			}
+		}
+    }
+  }
+  
